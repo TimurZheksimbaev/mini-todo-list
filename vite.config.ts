8edbe255614f10de3fd0fb8bba60c1d08311d4f4
Timur Zheksimbaev <113367706+TimurZheksimbaev@users.noreply.github.com>
2025-04-11 16:22:18 +0300

    Changed github pages build folder

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/mini-todo-list/', // Match your GitHub repo name
+  build: {
+    outDir: 'dist', // Explicitly set (this is Vite's default)
+  }
 })
